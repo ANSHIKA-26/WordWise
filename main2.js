e20892f4fb363a8271112680078ed89d51572680
Ekta bharti <123762263+Ek-ta-bharti@users.noreply.github.com>
2024-10-03 22:41:18 +0530

    03-10

--- main2.js
+++ main2.js
@@ -1,45 +1,12 @@
-let toggle = document.querySelector("#header .toggle-button");
-let collapse = document.querySelectorAll("#header .collapse");
-
-toggle.addEventListener('click' , function(){
-    collapse.forEach(col => col.classList.toggle("collapse-toggle"));
-})
-//swiper library
-// main.js
-document.addEventListener('DOMContentLoaded', function () {
-  var swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    autoplay: {
-      delay: 3000
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',  
-      prevEl: '.swiper-button-prev',
-    },
-  });
-});
-window.onscroll = function(){ myFunction()};
-
-// get the current value
-let navbar = document.getElementById("header");
-
-// get the navbar position
-let sticky = navbar.offsetTop;
-
-// sticky function
-function myFunction(){
-    if(window.pageYOffset >= sticky){
-        navbar.classList.add("sticky");
-    }else{
-        navbar.classList.remove("sticky");
-    }
-}
-
-
-
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     autoplay: {
+//         delay: 1000,
+//     },
+//     pagination: false,
+//     scrollbar: {
+//         el: null,
+//     },
+// });
