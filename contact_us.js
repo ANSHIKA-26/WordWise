6c5cc1831e84ab36392fa941a73dd92d24c4bdd4
Mayanksaininh <mayanksaininh@gmail.com>
2024-10-10 22:32:35 +0530

    Solved issue number #661

--- contact_us.js
+++ contact_us.js
@@ -61,3 +61,37 @@ async function SendEmail(event) {
     alert("Error sending email. Please try again later.");
   }
 }
+
+
+//  mobile No limit with contry code
+// ...........................................github->Mayanksaininh..................................................
+function getIp(callback) {
+  fetch('ipinfo.io/140.82.183.34?token=66e2f39b20a2bd', { headers: { 'Accept': 'application/json' }})
+    .then((resp) => resp.json())
+    .catch(() => {
+      return {
+        country: 'us',
+      };
+    })
+    .then((resp) => callback(resp.country));
+ }
+
+
+ 
+  const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+ 
+//  const info = document.querySelector(".alert-info");
+ 
+ function process(event) {
+  event.preventDefault();
+ 
+  const phoneNumber = phoneInput.getNumber();
+ 
+  info.style.display = "";
+  info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+ }
+//  ...............................................github->Mayanksaininh........................................................
