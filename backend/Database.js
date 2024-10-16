a8aaa4416efa312e0618d6d32ab875f4877c937a
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-10-16 20:52:23 +0530

    added otp based forgot password functionality

--- backend/Database.js
+++ backend/Database.js
@@ -4,8 +4,7 @@ async function ConnectDb() {
   try {
     // const DatabaseConnect = process.env.DatabaseConnect;
     await mongoose.connect(
-      //"mongodb://localhost:27017/" //establish database connection   // mongodb+srv://username:Password@cluster0.8ysl0ky.mongodb.net/DatabseName
-      "mongodb+srv://sourabhsinghrawat782:PzR2Sxd0SVViImI7@cluster0.jsmht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      process.env.MONGODB_URI
     );
 
     console.log("connected to database");
