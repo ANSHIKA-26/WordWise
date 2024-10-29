1748949f4e160e77e661a2c2fdd3b2f557423dc8
mansi066 <mansisinghal623@gmail.com>
2024-10-29 19:28:14 +0530

    Comment box added in all slides

---
+++ comment_box/comment.js
@@ -0,0 +1,24 @@
+let likeCount = 420;
+
+function toggleLike(element) {
+    if (element.classList.contains('active')) {
+        element.classList.remove('active');
+        likeCount--;
+    } else {
+        element.classList.add('active');
+        likeCount++;
+    }
+    element.querySelector('.count').innerText = likeCount;
+}
+
+function toggleDislike(element) {
+    element.classList.toggle('active');
+}
+
+document.querySelector('.like').addEventListener('click', function() {
+    toggleLike(this);
+});
+
+document.querySelector('.dislike').addEventListener('click', function() {
+    toggleDislike(this);
+});
